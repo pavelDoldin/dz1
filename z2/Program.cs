8e921f18c1594a2e5a2ff5d737aa751c78d4df2f
pavelDoldin <doldin86@icloud.com>
2022-11-25 18:34:09 +0500

    зфдача 2

---
+++ z2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод числа
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine()); // ввод числа
+if (n > m)
+Console.Write(n);
+else if (m > n)
+Console.Write(m);
+else if (m == n)
+Console.Write("числа равны"); 
