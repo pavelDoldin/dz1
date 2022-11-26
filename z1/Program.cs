bc755612a0a8a5a6a52a6e47e15491ff5b45904b
pavelDoldin <doldin86@icloud.com>
2022-11-26 17:08:55 +0500

    z1

--- z1/Program.cs
+++ z1/Program.cs
@@ -5,13 +5,9 @@ Console.Write("Введите 2-ое число: ");
 int m = Convert.ToInt32(Console.ReadLine()); // ввод числа
 Console.Write("Введите 3-те число: ");
 int v = Convert.ToInt32(Console.ReadLine()); // ввод числа
-if (n > m )
+if (n >= m && n >= v) 
 Console.Write(n);
-else if (m > n)
+else if (m >= n && m >= v)
 Console.Write(m);
-else if (v > n)
+else if (v >= n && v >= m)
 Console.Write(v);
-else if (v > m)
-Console.Write(v);
-else (m > v)
-Console.Write(m);
---
+++ z4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод числа
+for (int i = 2 ; i <= n ; i+=2 );
+    Console.Write($"{i} ");
