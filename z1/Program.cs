cb9ecc9e91749e8525adef3eadb8f2a81ef0de4e
pavelDoldin <doldin86@icloud.com>
2022-11-26 15:14:58 +0500

    z3

--- z1/Program.cs
+++ z1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод числа
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine()); // ввод числа
+Console.Write("Введите 3-те число: ");
+int v = Convert.ToInt32(Console.ReadLine()); // ввод числа
+if (n > m )
+Console.Write(n);
+else if (m > n)
+Console.Write(m);
+else if (v > n)
+Console.Write(v);
+else if (v > m)
+Console.Write(v);
+else (m > v)
+Console.Write(m);
---
+++ z3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввод числа
+if (n % 2 == 0 )
+Console.Write("yes");
+else 
+Console.Write("no");
